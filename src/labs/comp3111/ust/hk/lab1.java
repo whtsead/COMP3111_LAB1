75a1494194cf4b61b86aa197fc62ad60079d9885
whtsead <whtsead@csl4wk14.win2k.cs.ust.hk>
2018-02-12 16:25:48 +0800

    try2

--- src/labs/comp3111/ust/hk/lab1.java
+++ src/labs/comp3111/ust/hk/lab1.java
@@ -4,5 +4,7 @@ public class lab1 {
 
 	public static void main(String[]args){
 		System.out.print("Lab1 : Introduction to Git and Github");
+		System.out.print("Lab1 : Introduction to Git and Github2");
 	}
+	
 }
