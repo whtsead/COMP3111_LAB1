3e8199593780e759568ec612a4a14f7c305aac1b
whtsead <whtsead@csl4wk14.win2k.cs.ust.hk>
2018-02-12 17:10:20 +0800

    loop commit

--- src/labs/comp3111/ust/hk/lab1.java
+++ src/labs/comp3111/ust/hk/lab1.java
@@ -3,11 +3,13 @@ package labs.comp3111.ust.hk;
 public class lab1 {
 
 	public static void main(String[]args){
-		System.out.print("Lab1 : Introduction to Git and Github");
-		System.out.print("Lab1 : Introduction to Git and Github2");
-		System.out.print("Lab1 : Introduction to Git and Github3");
-		System.out.print("Lab1 : Introduction to Git and Github4");
-		System.out.print("Lab1 : Introduction to Git and Github5");
+		int [] arr = {1,2,3,4,5,6,7,8,9,10};
+		int sum = 0 ;
+		
+		for ( int i = 0 ; i < arr.length; i++){
+			sum += arr[i]; 
+		}
+		System.out.println(String.format("Sum of the number is %d", sum));
 	}
 	
 }
